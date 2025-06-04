52f9e562592dc3e1f11aec6b9e7bd3c45243c225
aapani-dukan <shivraj8404@gmail.com>
2025-06-05 00:22:10 +0530

    Create header.tsx

---
+++ client/src/components/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="p-4 bg-gray-100 shadow-md">
+      <h1 className="text-xl font-semibold">Shopnish</h1>
+    </header>
+  );
+}
